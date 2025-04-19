9833d0472c4a1508e74be3b605983f830ec09f0f
HanaHesham1 <119759653+HanaHesham1@users.noreply.github.com>
2025-04-19 13:37:27 +0000

    modifying eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,17 +1,26 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
   cluster_name    = "konecta-eks-cluster"
   cluster_version = "1.27"
-  subnets         = aws_subnet.private[*].id
-  vpc_id          = aws_vpc.main.id
 
-  node_groups = {
-    eks_nodes = {
+  subnet_ids         = aws_subnet.private[*].id
+  vpc_id             = aws_vpc.main.id
+
+  # Node group config
+  eks_managed_node_groups = {
+    dev-ng = {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
 
-      instance_type = "t3.small"
+      instance_types = ["t3.medium"]
     }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
