bf9f0139c07a3318bbd53699c3e8ca7377b0dd4b
HanaHesham1 <119759653+HanaHesham1@users.noreply.github.com>
2025-04-22 13:52:15 +0000

    triggering

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import os
 app = Flask(__name__)
 redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = 6379
-r = redis.Redis(host=redis_host, port=redis_port)
+redis_conn = redis.Redis(host=redis_host, port=redis_port)
 
 @app.route('/')
 def hello():
-    r.incr('hits')
-    return f'Hello from {os.getenv("ENVIRONMENT", "test")}! Hits: {r.get("hits").decode()}'
+    redis_conn.incr('counter')
+    return f"Hello from {os.getenv('ENVIRONMENT', 'unknown')}! Count: {redis_conn.get('counter').decode()}"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
